ef6f9efeac7b40d70ee1458ac875dc5ace236e28
Rhishav Lamichhane <110982820+SelAshyn@users.noreply.github.com>
2025-01-11 22:16:55 +0545

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,17 @@
 import GoogleProvider from 'next-auth/providers/google';
 import NextAuth from 'next-auth';
 
-const handler = NextAuth({ 
+const handler = NextAuth({
+    secret: process.env.NEXTAUTH_SECRET!, 
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+            authorization: {
+                params: {
+                    scope: "openid email profile",
+                },
+            }
         }),
     ],
 });
