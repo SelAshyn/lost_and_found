7df1841cc2961ce4a4cec2c788b29f6346826d6a
Rhishav Lamichhane <110982820+SelAshyn@users.noreply.github.com>
2025-01-11 22:00:34 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import { Bebas_Neue } from 'next/font/google';
 import "./style.css";
-import { signIn, useSession } from 'next-auth/react';
 
 const bebasneue = Bebas_Neue({
     subsets: ['latin'],
@@ -10,27 +9,12 @@ const bebasneue = Bebas_Neue({
   })
   
 export default function Home() {
-  const { data: session } = useSession();
 
   return (
       <div className="wel" style={bebasneue.style}>
-        {session?.user ? (
-          <div>
-            <h1>Welcome to SXC Lost && Found, <span style={{fontSize: '40px', color: 'aqua'}}>{session.user.name}</span></h1>
-            <h2>by <span style={{ color: 'red' }}>Student</span> for <span style={{ color: 'red' }}>Student</span></h2>
-          </div>
-        ) : (
-          <>
-            <h1>Welcome to SXC Lost && Found</h1>
-            <h2>by <span style={{ color: 'red' }}>Student</span> for <span style={{ color: 'red' }}>Student</span></h2>
-        <br />
-        <center>
-              <h3 className='signw'>
-                Please <a href="#" style={{color: 'yellow'}} onClick={() => signIn()}>signin</a> to continue.
-              </h3>
-            </center>
-          </>
-        )}
+          <h1>Welcome to Lost and Found</h1>
+          <p>Lost and Found is a platform that helps you find your lost items and also helps you return found items to their owners.</p>
+          <p>Sign in to get started</p>
         
       </div>
   );
